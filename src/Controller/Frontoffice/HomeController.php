92c963f207a4b49781d61c188536b9a4bf01c06b
David Renard <david.renard1@hotmail.fr>
2023-08-18 10:22:13 +0200

    commit after solving whoops/twig issue and reinstall composer

--- src/Controller/Frontoffice/HomeController.php
+++ src/Controller/Frontoffice/HomeController.php
@@ -21,37 +21,41 @@ class HomeController
         {
             $contactFormValidator= new ContactFormValidator($request, $this->session);
 
-            if ($contactFormValidator->isInputValid("/^[A-Za-z- _]+$/",$request->request()->get('firstname'))
-            && $contactFormValidator->isInputValid("/^[A-Za-z- _]+$/",$request->request()->get('name'))
-            && $contactFormValidator->isEmailValid($request->request()->get('email'))
-            && $contactFormValidator->isTextareaValid($request->request()->get('message'))
-            && $contactFormValidator->isRgpdChecked($request->request()->get('rgpd')))
+            $isFirstnameValid=$contactFormValidator->isInputValid("/^[A-Za-z- _]+$/",$request->request()->get('firstname'));
+            $isNameValid=$contactFormValidator->isInputValid("/^[A-Za-z- _]+$/",$request->request()->get('name'));
+            $isEmailValid=$contactFormValidator->isEmailValid($request->request()->get('email'));
+            $isMessageValid=$contactFormValidator->isTextareaValid($request->request()->get('message'));
+            $isRgpdChecked=$contactFormValidator->isRgpdChecked($request->request()->get('rgpd'));
+
+            if ($isFirstnameValid
+            && $isNameValid
+            && $isEmailValid
+            && $isMessageValid
+            && $isRgpdChecked)
             {
                 $this->session->addFlashes('success','Formulaire valide, votre message : "'.$request->request()->get("message").'" est bien envoyé!');
             }
 
-//            if ($contactFormValidator->isInputValid("/^[A-Za-z- _]+$/",$request->request()->get('firstname'))===false)
-//            {
-//                $this->session->addFlashes('errorFirstname',"Votre prénom ne peut pas contenir de caractères numériques ou autres caractères spéciaux (exceptés ' ','-' et '_')");
-//            }
-//            if ($contactFormValidator->isInputValid("/^[A-Za-z- _]+$/",$request->request()->get('name'))===false)
-//            {
-//                $this->session->addFlashes('errorName',"Votre nom ne peut pas contenir de caractères numériques ou autres caractères spéciaux (exceptés ' ','-' et '_')");
-//            }
-//            if ($contactFormValidator->isEmailValid($request->request()->get('email'))===false)
-//            {
-//                $this->session->addFlashes('errorEmail',"Votre email ne correspond pas");
-//            }
-//            if ($contactFormValidator->isTextareaValid($request->request()->get('message'))===false)
-//            {
-//                $this->session->addFlashes('errorMessage',"Votre message ne peut pas être vide, écrivez-nous quelque chose!");
-//            }
-//            if ($contactFormValidator->isRgpdChecked($request->request()->get('rgpd'))===false)
-//            {
-//                $this->session->addFlashes('errorRgpd',"Vous avez oublié la checkbox!");
-//            }
-
-            $this->session->addFlashes('error','Il y a au moins une erreur à la saisie du formulaire.');
+            if (!$isFirstnameValid)
+            {
+                $this->session->addFlashes('error',"Votre prénom ne peut pas contenir de caractères numériques ou autres caractères spéciaux (exceptés ' ','-' et '_')");
+            }
+            if (!$isNameValid)
+            {
+                $this->session->addFlashes('error',"Votre nom ne peut pas contenir de caractères numériques ou autres caractères spéciaux (exceptés ' ','-' et '_')");
+            }
+            if (!$isEmailValid)
+            {
+                $this->session->addFlashes('error',"Votre email ne correspond pas");
+            }
+            if (!$isMessageValid)
+            {
+                $this->session->addFlashes('error',"Votre message ne peut pas être vide, écrivez-nous quelque chose!");
+            }
+            if (!$isRgpdChecked)
+            {
+                $this->session->addFlashes('error',"Vous avez oublié la checkbox!");
+            }
         }
         return new Response($this->view->render(
             ["template" => 'home',
